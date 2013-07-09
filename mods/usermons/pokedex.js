2eedb249292ab1d5d81dad6b60de38af8731a5c0
Oiawesome <Oiawesome99@yahoo.com>
2013-07-09 16:03:42 -0700

    Create pokedex.js

---
+++ mods/usermons/pokedex.js
@@ -0,0 +1,3 @@
+exports.BattlePokedex = {
+oiawesome:{num:700,species:"Oiawesome",types:["Water","Electric"],gender:"N",baseStats:{hp:95,atk:95,def:100,spa:115,spd:90,spe:91},abilities:{0:"OBJECTION",1:"Drunken Fist",DW:"Drizzle"},heightm:0.4,weightkg:4,color:"Brown",eggGroups:["No Eggs"]},
+};

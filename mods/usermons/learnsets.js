06dfc3e0d333fcbbead3307354bd4bdfa4c453d2
Oiawesome <Oiawesome99@yahoo.com>
2013-07-09 16:02:55 -0700

    Create learnsets.js

---
+++ mods/usermons/learnsets.js
@@ -0,0 +1,3 @@
+exports.BattleLearnsets = {
+oiawesome:{learnset:{hydropump:["5L1"],surf:["5L1"],vodkabottlethrow:["5L1"],thunderbolt:["5L1"],thunder:["5L1"],thunderwave:["5L1"],waterspout:["4L1"],scald:["5L1"],discharge:["5L1"]}}
+};

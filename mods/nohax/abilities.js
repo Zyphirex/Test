0fd7903ead0db272f9e5c33906205ceacfbb7bdf
Pikachuun <pikachuun@gmail.com>
2013-07-10 12:12:19 -0400

    Screwing with Serenity

--- mods/nohax/abilities.js
+++ mods/nohax/abilities.js
@@ -1,17 +1,15 @@
 exports.BattleAbilities = {
   "serenegrace": {
-		desc: "The side effects of this Pokemon's attack occur twice as often. For example, if this Pokemon uses Ice Beam, it will have a 20% chance to freeze its target.",
-		shortDesc: "This Pokemon's moves have their secondary effect chance doubled.",
+  		inherit: true
 		onModifyMove: function(move) {
 			if (move.secondaries) {
 				this.debug('doubling secondary chance');
 				for (var i=0; i<move.secondaries.length; i++) {
-					move.secondaries[i].chance *= 2;
+					if move.secondaries[i].chance >= 50 {
+						move.secondaries[i].chance = 100;
+					}
 				}
 			}
-		},
-		id: "serenegrace",
-		name: "Serene Grace",
-		rating: 4,
-		num: 32
+		}
 	}
+};

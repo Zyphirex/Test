9fab449ed5180a1cf7c9d395e36615868e14dfdf
Pikachuun <pikachuun@gmail.com>
2013-07-11 15:58:11 -0400

    Skill Link Change

--- mods/nohax/abilities.js
+++ mods/nohax/abilities.js
@@ -11,5 +11,15 @@ exports.BattleAbilities = {
 				}
 			}
 		}
+	},
+	"skilllink": {
+		inherit: true
+		onModifyMove: function(move) {
+			if (move.multihit && move.multihit.length) {
+				if (move.multihit[1] === 3 && move.id !== "triplekick") {
+					move.multihit = 5;
+				}
+			}
+		}
 	}
 };

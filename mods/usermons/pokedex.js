863890e62cca5098b7b5e2c25bbef7e61499ad68
Pikachuun <pikachuun@gmail.com>
2013-07-11 13:11:35 -0400

    Le me

--- mods/usermons/pokedex.js
+++ mods/usermons/pokedex.js
@@ -1,4 +1,4 @@
 exports.BattlePokedex = {
 oiawesome:{num:700,species:"Oiawesome",types:["Water","Electric"],gender:"N",baseStats:{hp:95,atk:95,def:100,spa:115,spd:90,spe:91},abilities:{0:"OBJECTION",1:"Drunken Fist",DW:"Drizzle"},heightm:0.4,weightkg:4,color:"Brown",eggGroups:["No Eggs"]},
-pikachuun:{num:1337,species:"Who Needs a Species?!?",types:["Electric","Psychic"],gender:"M",baseStats:{hp:79,atk:117,def:72,spa:145,spd:104,spe:130},abilities:{0:"Pikachu Power",1:"Pikachu Sense",DW:"Regeneration II"},heightm:0.4,weightkg:6,color:"Yellow",eggGroups:["Field", "Fairy"]}
+pikachuun:{num:1337,species:"Pikachuun",types:["Electric","Psychic"],gender:"M",baseStats:{hp:79,atk:117,def:72,spa:145,spd:104,spe:130},abilities:{0:"Pikachu Power",1:"Pikachu Sense",DW:"Regeneration II"},heightm:0.4,weightkg:6,color:"Yellow",eggGroups:["Field", "Fairy"]}
 };

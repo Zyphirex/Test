80b81ed668ed2710e1ae0bed2057138752a31b33
Pikachuun <pikachuun@gmail.com>
2013-07-11 15:37:51 -0400

    Update formats-data.js

--- mods/usermons/formats-data.js
+++ mods/usermons/formats-data.js
@@ -7,11 +7,11 @@ exports.BattleFormatsData = {
 	pikachuun: {
 		viableMoves: {"thunderbolt":1,"volttackle":1,"voltswitch":1,"grassknot":1,"hiddenpowerice":1,"brickbreak":1,"extremespeed":1,"encore":1,"psychic":1,"psystrike":1,"substitute":1,"surf":1},
 		dreamWorldRelease: true,
-		tier: "NU"
+		tier: "OU"
 	},
 	snaquaza: {
 		viableMoves: {"snaquazabanpower":1},
 		dreamWorldRelease: true,
-		tier: "NU"
+		tier: "Ubers"
 	},
 };

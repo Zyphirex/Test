b4bfc1d74ffa331b7b2028ea785d66ffdf3043d2
Oiawesome <Oiawesome99@yahoo.com>
2013-07-11 10:24:38 -0700

    Update moves.js

--- mods/usermons/moves.js
+++ mods/usermons/moves.js
@@ -1,8 +1,8 @@
 exports.BattleMovedex = {
   	"vodkabottlethrow": {
    		num: 800,
-		accuracy: 85,
-		basePower: 20,
+		accuracy: 100,
+		basePower: 30,
 		category: "Special",
 		desc: "Deals damage to one adjacent target and hits two to five times. Has a 1/3 chance to hit two or three times, and a 1/6 chance to hit four or five times. If one of the hits breaks the target's Substitute, it will take damage for the remaining hits. If the user has the Ability Skill Link, this move will always hit five times. Makes contact.",
 		shortDesc: "Hits 2-5 times in one turn.",
@@ -11,7 +11,7 @@ exports.BattleMovedex = {
 		pp: 20,
 		priority: 0,
 		isContact: true,
-		multihit: [3,5],
+		multihit: [4,4],
 		secondary: false,
 		target: "normal",
 		type: "Water"

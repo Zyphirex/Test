786d7c091df046ec0571780af6202f0920bed4e3
Pikachuun <pikachuun@gmail.com>
2013-07-11 13:19:49 -0400

    Le me

--- mods/usermons/formats-data.js
+++ mods/usermons/formats-data.js
@@ -3,5 +3,10 @@ exports.BattleFormatsData = {
 		viableMoves: {"surf":1},
 		dreamWorldRelease: true,
 		tier: "OU"
+	},
+	pikachuun: {
+		viableMoves: {"thunderbolt":1,"volttackle":1,"voltswitch":1,"grassknot":1,"hiddenpowerice":1,"brickbreak":1,"extremespeed":1,"encore":1,"psychic":1,"psystrike":1,"substitute":1,"surf":1},
+		dreamWorldRelease: true,
+		tier: "NU"
 	}
 };

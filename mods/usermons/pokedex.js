bee9a84d9edba49180314fd882d1ced617495d5f
Oiawesome <Oiawesome99@yahoo.com>
2013-07-11 15:25:04 -0700

    Update pokedex.js

--- mods/usermons/pokedex.js
+++ mods/usermons/pokedex.js
@@ -1,5 +1,5 @@
 exports.BattlePokedex = {
-oiawesome:{num:700,species:"Oiawesome",types:["Water","Electric"],gender:"N",baseStats:{hp:95,atk:95,def:100,spa:115,spd:90,spe:91},abilities:{0:"OBJECTION",1:"Drunken Fist",DW:"Drizzle"},heightm:0.4,weightkg:4,color:"Brown",eggGroups:["No Eggs"]},
+oiawesome:{num:700,species:"Oiawesome",types:["Water","Fairy"],gender:"N",baseStats:{hp:95,atk:95,def:100,spa:115,spd:90,spe:91},abilities:{0:"OBJECTION",1:"Drunken Fist",DW:"Drizzle"},heightm:0.4,weightkg:4,color:"Brown",eggGroups:["No Eggs"]},
 pikachuun:{num:1337,species:"Pikachuun",types:["Electric","Psychic"],gender:"M",baseStats:{hp:77,atk:110,def:66,spa:121,spd:104,spe:121},abilities:{0:"Pikachu Power",1:"Pikachu Sense",DW:"Regeneration II"},heightm:0.4,weightkg:6,color:"Yellow",eggGroups:["Field", "Fairy"]},
 snaquaza:{num:7777,species:"Snaquaza",types:["Dark","Ghost,"],gender:"M",baseStats:{hp:50,atk:100,def:105,spa:150,spd:75,spe:110},abilities:{0:"Wonder Guard"},heightm:1000,weighting:60,color:"Blue",eggGroups:["Field"]}
 };

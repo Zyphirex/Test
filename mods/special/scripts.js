afe89dc74aa6775c103ea36519fc774ef5ca9b8f
Pikachuun <pikachuun@gmail.com>
2013-07-14 12:07:31 -0400

    3 minutes :L

--- mods/special/scripts.js
+++ mods/special/scripts.js
@@ -1,7 +1,12 @@
 exports.BattleScripts = {
 	init: function() {
 		for (var i in this.data.Pokedex) {
-			var sp = (this.data.Pokedex[i].baseStats.spa + this.data.Pokedex[i].baseStats.spd) / 2;
+			if (this.data.Pokedex[i].baseStats.spa > this.data.Pokedex[i].baseStats.spd) {
+				var sp = this.data.Pokedex[i].baseStats.spa
+			}
+			else {
+				var sp = this.data.Pokedex[i].baseStats.spd
+			}
 			this.data.Pokedex[i].baseStats.spa = sp;
 			this.data.Pokedex[i].baseStats.spd = sp;
 		}

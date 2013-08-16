309bd10680988edc1522231071d80aef0fc0631c
Oesher Ayenew <Oiawesome99@yahoo.com>
2013-08-16 11:22:52 -0700

    there was something wrong about previous scripts disabling other users from editing them

--- mods/sketchmons/scripts.js
+++
@@ -1,7 +0,0 @@
-exports.BattleScripts = {
-        init: function() {
-                for (var i in this.data.Learnset) {
-                        this.data.Learnsets[i].learnset.sketch = ["5E"];
-                }
-        }
-};

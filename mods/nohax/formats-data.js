e5df32c9c49061d04312b27c4f1ca7c865e038d6
Snaquaza <Snaquaza@Dragonfield.com>
2013-08-16 20:24:36 +0200

    THIS

---
+++ mods/nohax/formats-data.js
@@ -0,0 +1,10 @@
+shayminsky: {
+viableMoves: {"seedflare":1,"earthpower":1,"airslash":1,"hiddenpowerice":1,"hiddenpowerfire":1,"substitute":1,"leechseed":1},
+eventPokemon: [
+{"generation":4,"level":50,"moves":["seedflare","aromatherapy","substitute","energyball"],"pokeball":"cherishball"},
+{"generation":4,"level":30,"moves":["synthesis","leechseed","magicalleaf","growth"]},
+{"generation":4,"level":30,"moves":["growth","magicalleaf","leechseed","synthesis"]},
+{"generation":5,"level":50,"moves":["seedflare","leechseed","synthesis","sweetscent"],"pokeball":"cherishball"}
+],
+tier: "OU"
+},

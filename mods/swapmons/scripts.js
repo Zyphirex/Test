1dacffb7bfb47611ef0f986ea15a1c82221b6209
Snaquaza <Snaquaza@Dragonfield.com>
2013-08-26 16:15:36 +0200

    changes

--- mods/swapmons/scripts.js
+++ mods/swapmons/scripts.js
@@ -1,58 +1,58 @@
 exports.BattleScripts = {
     init: function() {
         for (var i in this.data.Pokedex) {
-            if (this.data.Pokedex[i].types[0] === 'Water') {
-                this.modData('Pokedex', i).types[0] = 'Grass';
+            if (this.data.Pokedex[i].types[1] === 'Water') {
+                this.modData('Pokedex', i).types[1] = 'Grass';
             } else if (this.data.Pokedex[i].types[1] === 'Water') {
                 this.modData('Pokedex', i).types[1] = 'Grass';
-
-            }
-else if (this.data.Pokedex[i].types[0] === 'Fire') {
-                this.modData('Pokedex', i).types[0] = 'Water';
 
             }
 else if (this.data.Pokedex[i].types[1] === 'Fire') {
                 this.modData('Pokedex', i).types[1] = 'Water';
 
             }
-else if (this.data.Pokedex[i].types[0] === 'Normal') {
-                this.modData('Pokedex', i).types[0] = 'Ghost';
+else if (this.data.Pokedex[i].types[1] === 'Fire') {
+                this.modData('Pokedex', i).types[1] = 'Water';
+
+            }
+else if (this.data.Pokedex[i].types[1] === 'Normal') {
+                this.modData('Pokedex', i).types[1] = 'Ghost';
 }
 else if (this.data.Pokedex[i].types[1] === 'Normal') {
                 this.modData('Pokedex', i).types[1] = 'Ghost';
 }
-else if (this.data.Pokedex[i].types[0] === 'Poison') {
-                this.modData('Pokedex', i).types[0] = 'Steel';
+else if (this.data.Pokedex[i].types[1] === 'Poison') {
+                this.modData('Pokedex', i).types[1] = 'Steel';
 }
 else if (this.data.Pokedex[i].types[1] === 'Poison') {
                 this.modData('Pokedex', i).types[1] = 'Steel';
 }
-else if (this.data.Pokedex[i].types[0] === 'Psychic') {
-                this.modData('Pokedex', i).types[0] = 'Fighting';
+else if (this.data.Pokedex[i].types[1] === 'Psychic') {
+                this.modData('Pokedex', i).types[1] = 'Fighting';
 }
 else if (this.data.Pokedex[i].types[1] === 'Psychic') {
                 this.modData('Pokedex', i).types[1] = 'Fighting';
 }
-else if (this.data.Pokedex[i].types[0] === 'Bug') {
-                this.modData('Pokedex', i).types[0] = 'Dragon';
+else if (this.data.Pokedex[i].types[1] === 'Bug') {
+                this.modData('Pokedex', i).types[1] = 'Dragon';
 }
 else if (this.data.Pokedex[i].types[1] === 'Bug') {
                 this.modData('Pokedex', i).types[1] = 'Dragon';
 }
-else if (this.data.Pokedex[i].types[0] === 'Ground') {
-                this.modData('Pokedex', i).types[0] = 'Flying';
+else if (this.data.Pokedex[i].types[1] === 'Ground') {
+                this.modData('Pokedex', i).types[1] = 'Flying';
 }
 else if (this.data.Pokedex[i].types[1] === 'Ground') {
                 this.modData('Pokedex', i).types[1] = 'Flying';
 }
-else if (this.data.Pokedex[i].types[0] === 'Electric') {
-                this.modData('Pokedex', i).types[0] = 'Dark';
+else if (this.data.Pokedex[i].types[1] === 'Electric') {
+                this.modData('Pokedex', i).types[1] = 'Dark';
 }
 else if (this.data.Pokedex[i].types[1] === 'Electric') {
                 this.modData('Pokedex', i).types[1] = 'Dark';
 }
-else if (this.data.Pokedex[i].types[0] === 'Ice') {
-                this.modData('Pokedex', i).types[0] = 'Rock';
+else if (this.data.Pokedex[i].types[1] === 'Ice') {
+                this.modData('Pokedex', i).types[1] = 'Rock';
 
             }
 else if (this.data.Pokedex[i].types[1] === 'Ice') {
@@ -185,7 +185,7 @@ else if (this.data.Learnsets[x].psyshock && this.modData('Pokedex', i).types[0] 
                     this.modData('Learnsets', x).focusblast = ['5L0'];
                 }
 else if (this.data.Learnsets[x].psystrike && this.modData('Pokedex', i).types[0] === 'Fighting' && x === i || this.data.Learnsets[x].psystrike && this.modData('Pokedex', i).types[1] === 'Fighting' && x === i) {
-                    this.modData('Learnsets', x).secret sword = ['5L0'];
+                    this.modData('Learnsets', x).secretsword = ['5L0'];
                 }
 else if (this.data.Learnsets[x].psychoboost && this.modData('Pokedex', i).types[0] === 'Fighting' && x === i || this.data.Learnsets[x].psychoboost && this.modData('Pokedex', i).types[1] === 'Fighting' && x === i) {
                     this.modData('Learnsets', x).closecombat = ['5L0'];
